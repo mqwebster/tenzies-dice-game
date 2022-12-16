ca59bb0ed47667e208e5224ddeac0ed774413779
Marques Webster <marques.q.webster@gmail.com>
2022-12-16 13:06:15 -0600

    hold and win states added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Dice from './Dice';
-// import Confetti from 'react-confetti/dist/types/Confetti';
+import Confetti from 'react-confetti';
 
 import './App.css';
 
 export default function App() {
   const [dice, setDice] = useState(newDice());
-  const [hold, setHold] = useState([]);
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const win = (arr) => {
+      return arr.every((item) => item.isHeld && item.num === arr[0].num);
+    };
+
+    setTenzies(win(dice));
+  }, [dice]);
 
   function newDice() {
     const diceArray = [];
@@ -24,18 +32,52 @@ export default function App() {
   }
 
   function diceRoll() {
-    setDice(newDice());
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        const newNum = Math.ceil(Math.random() * 6);
+        return die.isHeld ? { ...die } : { ...die, num: newNum };
+      })
+    );
+  }
+
+  function diceHold(diceId) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return diceId === die.id ? { ...die, isHeld: !die.isHeld } : { ...die };
+      })
+    );
   }
 
   const diceElements = dice.map((item) => {
-    return <Dice key={item.id} num={item.num} />;
+    return (
+      <Dice
+        key={item.id}
+        id={item.id}
+        num={item.num}
+        isHeld={item.isHeld}
+        diceHold={() => diceHold(item.id)}
+      />
+    );
   });
 
   return (
     <main>
+      {tenzies && <Confetti />}
+
+      <div className='game'>
+        <h1>Tenzies</h1>
+        <p>
+          Roll untill all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+      </div>
+
       <div className='dice-grid'>{diceElements}</div>
-      <button className='dice-roll' onClick={diceRoll}>
-        Roll
+      <button
+        className='dice-roll'
+        onClick={() => (tenzies ? setDice(newDice()) : diceRoll())}
+      >
+        {tenzies ? 'New Game' : 'Roll'}
       </button>
     </main>
   );
--- src/Dice.jsx
+++ src/Dice.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 export default function Dice(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? '#59e391' : '#ffffff',
+  };
   return (
-    <div className='dice'>
+    <div className='dice' style={styles} onClick={props.diceHold}>
       <span>{props.num}</span>
     </div>
   );

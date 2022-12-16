f06237301a7994f464d652f74111ceaac0615a75
Marques Webster <marques.q.webster@gmail.com>
2022-12-15 22:19:07 -0600

    initial random dice setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
 import { useState } from 'react';
-import Confetti from 'react-confetti/dist/types/Confetti';
+import Dice from './Dice';
+// import Confetti from 'react-confetti/dist/types/Confetti';
+
 import './App.css';
+
+export default function App() {
+  const diceArray = [];
+  for (let i = 0; i < 10; i++) {
+    const singleDice = {
+      id: '',
+      num: '',
+    };
+    singleDice.id = i + 1;
+    singleDice.num = Math.ceil(Math.random() * 6);
+    diceArray.push(singleDice);
+  }
+
+  const diceElements = diceArray.map((item) => {
+    return <Dice key={item.id} num={item.num} />;
+  });
+
+  return (
+    <main>
+      <div className='dice-grid'>{diceElements}</div>
+    </main>
+  );
+}
---
+++ src/Dice.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Dice(props) {
+  return (
+    <div className='dice'>
+      <span>{props.num}</span>
+    </div>
+  );
+}
